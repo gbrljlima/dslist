378426d2c50d7608872e46545b6024e158ca673b
Gabriel Lima <124406585+gbrljlima@users.noreply.github.com>
2023-11-17 18:49:37 -0300

    Game repository, service, controller

---
+++ src/main/java/com/intensivao/dslist/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.intensivao.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.intensivao.dslist.dto.GameMinDTO;
+import com.intensivao.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll() {
+		var result = gameService.findAll();
+		return result;
+	}
+}
