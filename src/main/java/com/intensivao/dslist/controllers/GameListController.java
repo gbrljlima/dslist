07b1dd855b5ab168c0a22c1d2aa2bda508fbd084
Gabriel Lima <124406585+gbrljlima@users.noreply.github.com>
2023-11-20 13:24:07 -0300

    Get game by lists

--- src/main/java/com/intensivao/dslist/controllers/GameListController.java
+++ src/main/java/com/intensivao/dslist/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.intensivao.dslist.dto.GameListDTO;
+import com.intensivao.dslist.dto.GameMinDTO;
 import com.intensivao.dslist.services.GameListService;
+import com.intensivao.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -17,9 +20,18 @@ public class GameListController {
 	@Autowired
 	private GameListService gameListService;
 	
+	@Autowired
+	private GameService gameService;
+	
 	@GetMapping
 	public List<GameListDTO> findAll() {
 		List<GameListDTO> result = gameListService.findAll();
+		return result;
+	}
+	
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable long listId) {
+		List<GameMinDTO> result = gameService.findByList(listId);
 		return result;
 	}
 }
